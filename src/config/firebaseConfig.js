855843d0513795cb0eee3ec33fc6822ad34452e7
AggelosDiama <aggelosdiama@gmail.com>
2024-02-01 22:31:14 +0200

    delete project info from git

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,19 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Load environment variables from .env file
+const apiKey = process.env.API_KEY;
+const authDomain = process.env.AUTH_DOMAIN;
+const databaseURL = process.env.DATABASE_URL;
+const projectId = process.env.PROJECT_ID;
+const storageBucket = process.env.STORAGE_BUCKET;
+const messagingSenderId = process.env.MESSAGING_SENDER_ID;
+const appId = process.env.APP_ID;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAdrmtigXisK1_l5x6YQ5s2Fg3QARctuQo",
-  authDomain: "thesis-77e2b.firebaseapp.com",
-  databaseURL:
-    "https://thesis-77e2b-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "thesis-77e2b",
-  storageBucket: "thesis-77e2b.appspot.com",
-  messagingSenderId: "229809480316",
-  appId: "1:229809480316:web:960c1d9639f9d2722e9ff9",
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-//export default app;
